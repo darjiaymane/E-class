9fc6b00ab9c1f90a1950533d580a39af3fb2e3f4
darjiaymane <aymanedarji7@gmail.com>
2022-02-15 14:59:24 +0100

    start the crud  (add students option by the post method)

---
+++ students/add.php
@@ -0,0 +1,22 @@
+<?php
+    include '../operations.php';
+        if(isset($_POST['submit'])){
+            $name = $_POST['Name'];
+            $email = $_POST['Email'];
+            $phone = $_POST['Phone'];
+            $enrollNumber = $_POST['EnrollNumber'];
+            $dateOfAdmission = $_POST['DateOfAdmission'];
+
+            $sql_insert = "INSERT INTO students(Name, Email, Phone, EnrollNumber, DateOfAdmission) VALUES ('$name', '$email', '$phone', '$enrollNumber', '$dateOfAdmission')";
+            $REQ = mysqli_query($conn, $sql_insert);
+
+            if($REQ){
+                echo "Student record is successfully inserted!";
+                header("location:../students.php");
+                
+            }
+            else {
+                echo "something went wrong!";
+            }
+        }
+    ?>
