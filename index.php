c69c415489e501a2863ead2376cde41be4478347
aymane <aymanedarj7@gmail.com>
2022-03-17 18:44:01 +0100

    Add some modification to the form style and link it to the Signup page

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body class="bg-primary index">
     <main class=" vh-100 sm d-flex justify-content-center align-items-center">
         <section
-            class=" sm rounded-3 pt-5 pb-5 ps-4 pe-4 w-35 bg-white text-dark d-flex flex-column align-items-center">
+            class=" sm rounded-3 pt-5 pb-4 ps-4 pe-4 w-35 bg-white text-dark d-flex flex-column align-items-center">
             <form action="valide.php" method="POST" class="w-100">
                 <div class="text-start w-100 border-start ms-5 mb-5">
                     <h2 class="fw-bold  ms-2">
@@ -40,7 +40,7 @@
                  //  $lolo=$_COOKIE["email"];
                  // echo $lolo;
                 ?>
-                <div class="mb-4 pb-2">
+                <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label mb-2 text-muted">Password</label>
                     <input type="password"  name="password"  class="form-control p-2 border-light" placeholder="Enter your password" id="exampleInputPassword1" value="<?php if (isset($_COOKIE['password'])){echo ($_COOKIE['password']);}?>">
                     <p class="alert alert-danger fs-11 border-0 text-center mt-2" role="alert">
@@ -50,7 +50,7 @@
                         ?>
                     </p>
                 </div>
-                <div class="mb-3 form-check d-flex align-items-center">
+                <div class="mb-3 ms-2 form-check d-flex align-items-center">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1" name="remember">
                     <label class="form-check-label ms-2"  for="exampleCheck1">Remember me</label>
                 </div>
@@ -60,6 +60,7 @@
             </form>
             <div>
                 <p class="text-black-50">Forgot your password? <a href="" class="text-primary">Reset Password</a> </p>
+                <p class="text-black-50 mt-2 text-center">Create new account? <a href="signup.php" class="text-primary">Sign up</a> </p
             </div>
         </section>
     </main>
