866b3fcbd0a409a7fa6d8c6e64d982c578c3cbe0
aymane <aymanedarj7@gmail.com>
2022-03-17 19:44:30 +0100

    Add the message (username, email) error to the signup page, and style the labels

--- signup.php
+++ signup.php
@@ -1,9 +1,10 @@
 <?php
+session_start();
 $path="";
 include './DecoupFiles/head.php';
 include 'operations.php';
 ?>
-<body>
+<body class="bg-primary index">
 <main class=" vh-100 sm d-flex justify-content-center align-items-center">
     <section class=" sm rounded-3 pt-4 pb-4 ps-4 pe-4 w-35 bg-white text-dark d-flex flex-column align-items-center">
             <form class="p-3" method="POST" action="./students/add.php">
@@ -15,28 +16,31 @@ include 'operations.php';
                 <div class="text-center mb-3">
                     <h4>SIGN UP</h4>
                     <p class="text-black-50">Créer votre compte E-classe Administrator</p>
-
+                    <small class="text-danger"><?php
+                        if(isset($_SESSION['error4'])){
+                            echo $_SESSION['error4'] ;}
+                        ?></small>
                 </div>
-                <div class="d-flex">
+                <div class="d-flex flex-column flex-sm-row gap-3">
                     <div class="mb-3 w-100">
-                        <label for="fname" class="col-form-label">First Name</label>
+                        <label for="fname" class="form-label  text-muted">First Name</label>
                         <input class="form-control p-1" type="text" id="fname" name="Fname" required/>
                     </div>
                     <div class="mb-3 w-100">
-                        <label for="lname" class="col-form-label">Last Name</label>
+                        <label for="lname" class="form-label  text-muted">Last Name</label>
                         <input class="form-control p-1" id="lname" type="text" name="Lname" required/>
                     </div>
                 </div>
                  <div class="mb-3">
-                    <label for="username" class="col-form-label">Username</label>
+                    <label for="username" class="form-label  text-muted">Username</label>
                     <input class="form-control p-1" type="text" id="username" name="username" required/>
                 </div>
                 <div class="mb-3">
-                    <label for="email"  class="col-form-label">Email</label>
+                    <label for="email"  class="form-label  text-muted">Email</label>
                     <input class="form-control p-1" type="email" id="email" name="email" required/>
                 </div>
                 <div class="mb-3">
-                    <label for="password" class="col-form-label">Password</label>
+                    <label for="password" class="form-label  text-muted">Password</label>
                     <input class="form-control p-1" id="password" type="password" name="password" required/>
                 </div>
                 <div class="text-center mb-3">
