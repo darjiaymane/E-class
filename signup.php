4946842afc8efde15748c998a83103166bc94733
aymane <aymanedarj7@gmail.com>
2022-03-21 15:58:16 +0100

    Add the  errors message to the signup page, and link the script

--- signup.php
+++ signup.php
@@ -7,7 +7,7 @@ include 'operations.php';
 <body class="bg-primary index">
 <main class=" vh-100 sm d-flex justify-content-center align-items-center">
     <section class=" sm rounded-3 pt-4 pb-4 ps-4 pe-4 w-35 bg-white text-dark d-flex flex-column align-items-center">
-            <form class="p-3" method="POST" action="./students/add.php">
+            <form name="signup" class="p-3" onsubmit="validate(event)" method="POST" action="./students/add.php">
                 <div class="text-start w-100 border-start ms-5 mb-3">
                     <h2 class="fw-bold  ms-2">
                         E-classe
@@ -24,24 +24,29 @@ include 'operations.php';
                 <div class="d-flex flex-column flex-sm-row gap-3">
                     <div class="mb-3 w-100">
                         <label for="fname" class="form-label  text-muted">First Name</label>
-                        <input class="form-control p-1" type="text" id="fname" name="Fname" required/>
+                        <input class="form-control p-1" type="text" id="fname" name="Fname"/>
+                        <small class="alert alert-danger fs-9 border-0" id="fnameError"></small>
                     </div>
                     <div class="mb-3 w-100">
                         <label for="lname" class="form-label  text-muted">Last Name</label>
-                        <input class="form-control p-1" id="lname" type="text" name="Lname" required/>
+                        <input class="form-control p-1" id="lname" type="text" name="Lname" />
+                        <small class="alert alert-danger fs-9 border-0" id="lnameError"></small>
                     </div>
                 </div>
                  <div class="mb-3">
                     <label for="username" class="form-label  text-muted">Username</label>
-                    <input class="form-control p-1" type="text" id="username" name="username" required/>
+                    <input class="form-control p-1" type="text" id="username" name="username" />
+                     <small class="alert alert-danger fs-9 border-0" id="usernameError"></small>
                 </div>
                 <div class="mb-3">
                     <label for="email"  class="form-label  text-muted">Email</label>
-                    <input class="form-control p-1" type="email" id="email" name="email" required/>
+                    <input class="form-control p-1" type="email" id="email" name="email" />
+                    <small class="alert alert-danger fs-9 border-0" id="emailError"></small>
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label  text-muted">Password</label>
-                    <input class="form-control p-1" id="password" type="password" name="password" required/>
+                    <input class="form-control p-1" id="password" type="password" name="password" />
+                    <small class="alert alert-danger fs-9 border-0" id="passError"></small>
                 </div>
                 <div class="text-center mb-3">
                     <button type="submit" name="signup" class="btn btn-primary w-100 p-2">SIGN UP</button>
@@ -52,6 +57,7 @@ include 'operations.php';
             </div>
     </section>
 </main>
+<script src="main.js"></script>
 <?php
     include 'DecoupFiles/footer.php';
     session_destroy();
